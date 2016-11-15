2588061d580c97a044db916b867c45dcdec39926
xqf <xqf@eeepay.cn>
2016-11-15 17:30:49 +0800

    [update] 添加些menu方法

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 toolbar项目结构介绍
 ===================
 重点参考
-https://github.com/xuqingfeng77/AndroidSystemUiTraining/blob/master/note/01_AndroidSystemUI：ToolBar特性的使用.md
+https://github.com/xuqingfeng77/AndroidSystemUiTraining/blob/master/note/01_AndroidSystemUI：ToolBar特性的使用.md<\br>
 
 
+http://www.codeceo.com/article/android-toolbar-develop.html<\br>
--- app/src/main/java/com/eposp/toolbartest/MainActivity.java
+++ app/src/main/java/com/eposp/toolbartest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -37,6 +38,32 @@ public class MainActivity extends BaseActivity {
 //        toolbar.setSubtitleTextColor(getResources().getColor(android.R.color.darker_gray));//设置子标题颜色
         toolbar.setSubtitleTextAppearance(this, R.style.Theme_ToolBar_Base_Subtitle);//设置子标题的外观，包括文字颜色，文字大小等
         Log.d("TAG","测试修改内冲突的情况");
+
+        /**
+         * 修改更多的图片是有方法的
+         * 几天的参考http://www.codeceo.com/article/android-toolbar-develop.html
+         */
+        toolbar.inflateMenu(R.menu.base_toolbar_menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int menuItemId = item.getItemId();
+                if (menuItemId == R.id.action_search) {
+                    Toast.makeText(MainActivity.this , R.string.menu_search , Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_notification) {
+                    Toast.makeText(MainActivity.this , R.string.menu_notifications , Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_item1) {
+                    Toast.makeText(MainActivity.this , R.string.item_01 , Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_item2) {
+                    Toast.makeText(MainActivity.this , R.string.item_02 , Toast.LENGTH_SHORT).show();
+
+                }
+                return true;
+            }
+        });
     }
 
     @Override
